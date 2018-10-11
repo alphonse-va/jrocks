5a9ed184c9788dabb26138754eddbcb7da3bc2a3
ava <alcapcom@gmail.com>
2018-10-11 17:28:01 +0200

    add samples project

---
+++ jrocks-samples/src/main/java/jrocks/samples/model/MatrixWithoutDefaultConstructor.java
@@ -0,0 +1,10 @@
+package jrocks.samples.model;
+
+public class MatrixWithoutDefaultConstructor {
+
+  private String test;
+
+  public MatrixWithoutDefaultConstructor(String test) {
+    this.test = test;
+  }
+}
