1bc695fb7031076da5465b7d535c6e8291765902
ava <alcapcom@gmail.com>
2018-10-26 18:14:10 +0200

    remove test classes

--- jrocks-package/install.sh
+++ jrocks-package/install.sh
@@ -8,19 +8,19 @@ mvn package spring-boot:repackage -D skipTests
 
 if [ -f ~/.bashrc ]; then
   if grep -q "jrocks_alias" ~/.bashrc; then
-    echo "alias jrocks='java -jar `pwd`/target/jrocks-core*.jar'" > $ALIAS_FILENAME 
+    echo "alias jrocks='java -jar `pwd`/target/jrocks*.jar'" > ${ALIAS_FILENAME}
   else
     echo -e "\n# JRocks\nif [ -f ~/.jrocks_alias ]; then \n. ~/.jrocks_alias\nfi" >> ~/.bashrc
-    echo "alias jrocks='java -jar `pwd`/target/jrocks-core*.jar'" > $ALIAS_FILENAME 
+    echo "alias jrocks='java -jar `pwd`/target/jrocks*.jar'" > ${ALIAS_FILENAME}
   fi
 fi
 
 if [ -f ~/.zshrc ]; then
   if grep -q "jrocks_alias" ~/.zshrc; then
-    echo "alias jrocks='java -jar `pwd`/target/jrocks*.jar'" > $ALIAS_FILENAME 
+    echo "alias jrocks='java -jar `pwd`/target/jrocks*.jar'" > ${ALIAS_FILENAME}
   else
     echo -e "\n# JRocks\nif [ -f ~/.jrocks_alias ]; then \n. ~/.jrocks_alias\nfi" >> ~/.zshrc
-    echo "alias jrocks='java -jar `pwd`/target/jrocks-core*.jar'" > $ALIAS_FILENAME 
+    echo "alias jrocks='java -jar `pwd`/target/jrocks*.jar'" > ${ALIAS_FILENAME}
   fi
 fi
 
